9932eb59716c5936228267b58470504853ea843c
alexm356 <alexmondro01@gmail.com>
2024-02-26 15:06:05 -0800

    first

---
+++ main.c
@@ -0,0 +1,33 @@
+#include  <stdio.h>
+
+void move();
+
+
+int state;
+
+int main(){
+
+    int players;
+
+    int board[8][8] =   {{1, 1, 1, 1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                        {1, 1, 1, 1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1, 1, 1, 1}};
+
+
+    printf("Enter amount of players: ");
+    scanf("%d", &players);
+
+    if (players == )
+
+
+}
+
+void move(){
+
+
+
+}
